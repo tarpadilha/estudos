ab316814c832f5b08dbe8a604fc4fcf1cb8db9b7
tarpadilha <111711135+tarpadilha@users.noreply.github.com>
2024-09-14 09:13:53 -0300

    Estudo de operador new

--- operador-new002.js
+++ operador-new002.js
@@ -1,1 +1,23 @@
-console.log('Operador new')
+/*
+let obj1 = {nome: 'José', idade: 13, endereco:'Rua Braz do Amaral', numeroEndereco: 51}
+
+numeroEndereco = 12
+
+console.log(obj1)
+console.log(numeroEndereco)
+console.log(typeof obj1.numeroEndereco)
+*/
+
+function Pessoa() {
+
+}
+
+const joao = Pessoa()
+const maria = new Pessoa()
+
+// const obj2 = {}
+// return obj
+
+console.log(joao)
+console.log(typeof maria)
+//console.log(obj2)
